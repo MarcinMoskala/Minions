70e6a4386dcd571b5717b547f49cc8ad95d54bd9
MarcinMoskala <marcinmoskala@gmail.com>
2016-12-13 14:36:59 +0100

    Update extras

--- app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
+++ app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
@@ -10,12 +10,12 @@ import org.joda.time.LocalDate
 import java.io.Serializable
 import kotlin.LazyThreadSafetyMode.NONE
 
-fun <T : Parcelable> Activity.extra(key: String, default: T? = null) = lazy(NONE) { intent?.extras?.getParcelable<T>(key) ?: default }
+fun <T : Parcelable> Activity.extra(key: String, default: T? = null): Lazy<T> = lazy(NONE) { intent?.extras?.getParcelable<T>(key) ?: default ?: throw Error("No value $key") }
 fun <T : Serializable?> Activity.serializableExtra(key: String, default: T): Lazy<T> = lazy(NONE) { (intent?.extras?.getSerializable(key) as? T) ?: default }
-fun Activity.extraBoolean(key: String, default: Boolean) = lazy(NONE) { intent?.extras?.getBoolean(key, default) ?: default }
-fun Activity.extraInt(key: String, default: Int? = null) = lazy(NONE) { intent?.extras?.getInt(key) ?: default }
+fun Activity.extraBoolean(key: String, default: Boolean): Lazy<Boolean> = lazy(NONE) { intent?.extras?.getBoolean(key, default) ?: default }
+fun Activity.extraInt(key: String, default: Int = -1): Lazy<Int> = lazy(NONE) { intent?.extras?.getInt(key) ?: default }
+fun Activity.extraDateTime(key: String, default: DateTime? = null): Lazy<DateTime> = lazy(NONE) { intent?.extras?.getString(key)?.parseDateTime() ?: default ?: throw Error("No value $key") }
+fun Activity.stringExtra(key: String, default: String? = null): Lazy<String> = lazy(NONE) { intent?.extras?.getString(key) ?: default ?: throw Error("No value $key") }
 
 fun <T : Parcelable> Fragment.extra(key: String, default: T? = null) = lazy(NONE) { arguments?.getParcelable<T>(key) ?: default }
 fun Fragment.extraLocalDate(key: String, default: LocalDate? = null) = lazy(NONE) { arguments?.getCharSequence(key).toString().parseLocalDate() ?: default }
-fun Activity.extraDateTime(key: String, default: DateTime? = null) = lazy(NONE) { intent?.extras?.getString(key)?.parseDateTime() ?: default }
-fun Activity.stringExtra(key: String, default: String? = null) = lazy(NONE) { intent?.extras?.getString(key) ?: default }
