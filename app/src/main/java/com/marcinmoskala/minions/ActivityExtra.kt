aefb3c1950e5e356a855a3c425eb6480e7443299
MarcinMoskala <marcinmoskala@gmail.com>
2016-12-13 12:29:11 +0100

    Default values on extras

--- app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
+++ app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
@@ -5,15 +5,17 @@ package com.marcinmoskala.minions
 import android.app.Activity
 import android.support.v4.app.Fragment
 import android.os.Parcelable
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
 import java.io.Serializable
 import kotlin.LazyThreadSafetyMode.NONE
 
-fun <T : Parcelable> Activity.extra(key: String) = lazy(NONE) { intent.extras.getParcelable<T>(key) }
-fun <T : Serializable?> Activity.serializableExtra(key: String, default: T): Lazy<T> = lazy(NONE) { (intent.extras.getSerializable(key) as? T) ?: default }
-fun Activity.extraBoolean(key: String, default: Boolean) = lazy(NONE) { intent.extras.getBoolean(key, default) }
-fun Activity.extraInt(key: String) = lazy(NONE) { intent.extras.getInt(key) }
+fun <T : Parcelable> Activity.extra(key: String, default: T? = null) = lazy(NONE) { intent?.extras?.getParcelable<T>(key) ?: default }
+fun <T : Serializable?> Activity.serializableExtra(key: String, default: T): Lazy<T> = lazy(NONE) { (intent?.extras?.getSerializable(key) as? T) ?: default }
+fun Activity.extraBoolean(key: String, default: Boolean) = lazy(NONE) { intent?.extras?.getBoolean(key, default) ?: default }
+fun Activity.extraInt(key: String, default: Int? = null) = lazy(NONE) { intent?.extras?.getInt(key) ?: default }
 
-fun <T : Parcelable> Fragment.extra(key: String) = lazy(NONE) { arguments.getParcelable<T>(key) }
-fun Fragment.extraLocalDate(key: String) = lazy(NONE) { arguments.getCharSequence(key).toString().parseLocalDate() }
-fun Activity.extraDateTime(key: String) = lazy(NONE) { intent.extras.getString(key).parseDateTime() }
-fun Activity.stringExtra(key: String) = lazy(NONE) { intent.extras.getString(key) }
+fun <T : Parcelable> Fragment.extra(key: String, default: T? = null) = lazy(NONE) { arguments?.getParcelable<T>(key) ?: default }
+fun Fragment.extraLocalDate(key: String, default: LocalDate? = null) = lazy(NONE) { arguments?.getCharSequence(key).toString().parseLocalDate() ?: default }
+fun Activity.extraDateTime(key: String, default: DateTime? = null) = lazy(NONE) { intent?.extras?.getString(key)?.parseDateTime() ?: default }
+fun Activity.stringExtra(key: String, default: String? = null) = lazy(NONE) { intent?.extras?.getString(key) ?: default }
