8ad2779c58beb45a1b9df4b3b798d150690db1c7
MarcinMoskala <marcinmoskala@gmail.com>
2016-12-19 14:24:58 +0100

    Change global extra

--- app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
+++ app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
@@ -11,6 +11,7 @@ import java.io.Serializable
 import kotlin.LazyThreadSafetyMode.NONE
 
 fun <T : Parcelable> Activity.extra(key: String, default: T? = null): Lazy<T> = lazy(NONE) { intent?.extras?.getParcelable<T>(key) ?: default ?: throw Error("No value $key") }
+fun <T : Parcelable> Activity.extraNullable(key: String, default: T? = null): Lazy<T?> = lazy(NONE) { intent?.extras?.getParcelable<T>(key) ?: default ?: throw Error("No value $key") }
 fun <T : Serializable?> Activity.serializableExtra(key: String, default: T): Lazy<T> = lazy(NONE) { (intent?.extras?.getSerializable(key) as? T) ?: default }
 fun Activity.extraBoolean(key: String, default: Boolean): Lazy<Boolean> = lazy(NONE) { intent?.extras?.getBoolean(key, default) ?: default }
 fun Activity.extraInt(key: String, default: Int = -1): Lazy<Int> = lazy(NONE) { intent?.extras?.getInt(key) ?: default }
--- test/src/main/java/com/marcinmoskala/test/MainActivity.kt
+++ test/src/main/java/com/marcinmoskala/test/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.marcinmoskala.test
 
-import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.marcinmoskala.minions.bindToPreferenceField
-import com.marcinmoskala.minions.preferences
-import com.marcinmoskala.minions.toast
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v7.app.AppCompatActivity
+import com.marcinmoskala.minions.extra
+import com.marcinmoskala.minions.extraNullable
+
 
 class MainActivity : AppCompatActivity() {
+
+//    val a: Parcelable by extra("A")
+//    val b: Parcelable? by extraNullable("A")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
