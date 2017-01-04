08f5b469af7f2b32eaaf08754c54c8bd7ed234b5
MarcinMoskala <marcinmoskala@gmail.com>
2017-01-04 12:34:32 +0100

    add bundle for

--- app/src/main/java/com/marcinmoskala/minions/StartActivity.kt
+++ app/src/main/java/com/marcinmoskala/minions/StartActivity.kt
@@ -60,3 +60,43 @@ fun fillIntentArguments(intent: Intent, params: List<Pair<String, Any>>) {
         }
     }
 }
+
+inline fun <reified T: Activity> Context.bundleFor(vararg params: Pair<String, Any?>): Bundle {
+    val bundle = Bundle()
+    if (params.isNotEmpty()) fillBundleArguments(bundle, params.filter { it.second != null }.map { it.first to it.second!! })
+    return bundle
+}
+
+fun fillBundleArguments(bundle: Bundle, params: List<Pair<String, Any>>) {
+    params.forEach {
+        val value = it.second
+        when (value) {
+            is Int -> bundle.putInt(it.first, value)
+            is Long -> bundle.putLong(it.first, value)
+            is CharSequence -> bundle.putCharSequence(it.first, value)
+            is String -> bundle.putString(it.first, value)
+            is Float -> bundle.putFloat(it.first, value)
+            is Double -> bundle.putDouble(it.first, value)
+            is Char -> bundle.putChar(it.first, value)
+            is Short -> bundle.putShort(it.first, value)
+            is Boolean -> bundle.putBoolean(it.first, value)
+            is Serializable -> bundle.putSerializable(it.first, value)
+            is Bundle -> bundle.putBundle(it.first, value)
+            is Parcelable -> bundle.putParcelable(it.first, value)
+            is Array<*> -> when {
+                value.isArrayOf<CharSequence>() -> bundle.putCharSequenceArray(it.first, value as Array<CharSequence>)
+                value.isArrayOf<String>() -> bundle.putStringArray(it.first, value as Array<String>)
+                value.isArrayOf<Parcelable>() -> bundle.putParcelableArray(it.first, value as Array<Parcelable>)
+                else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+            }
+            is IntArray -> bundle.putIntArray(it.first, value)
+            is LongArray -> bundle.putLongArray(it.first, value)
+            is FloatArray -> bundle.putFloatArray(it.first, value)
+            is DoubleArray -> bundle.putDoubleArray(it.first, value)
+            is CharArray -> bundle.putCharArray(it.first, value)
+            is ShortArray -> bundle.putShortArray(it.first, value)
+            is BooleanArray -> bundle.putBooleanArray(it.first, value)
+            else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+        }
+    }
+}
