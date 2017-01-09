1e70629fa2a9c4847bdce3c9cb0de0492ccc13bb
MarcinMoskala <marcinmoskala@gmail.com>
2017-01-09 17:06:23 +0100

    Remove formats

--- app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
+++ app/src/main/java/com/marcinmoskala/minions/ActivityExtra.kt
@@ -3,10 +3,8 @@
 package com.marcinmoskala.minions
 
 import android.app.Activity
-import android.support.v4.app.Fragment
 import android.os.Parcelable
-import org.joda.time.DateTime
-import org.joda.time.LocalDate
+import android.support.v4.app.Fragment
 import java.io.Serializable
 import kotlin.LazyThreadSafetyMode.NONE
 
@@ -15,8 +13,5 @@ fun <T : Parcelable> Activity.extraNullable(key: String, default: T? = null): La
 fun <T : Serializable?> Activity.serializableExtra(key: String, default: T): Lazy<T> = lazy(NONE) { (intent?.extras?.getSerializable(key) as? T) ?: default }
 fun Activity.extraBoolean(key: String, default: Boolean): Lazy<Boolean> = lazy(NONE) { intent?.extras?.getBoolean(key, default) ?: default }
 fun Activity.extraInt(key: String, default: Int = -1): Lazy<Int> = lazy(NONE) { intent?.extras?.getInt(key) ?: default }
-fun Activity.extraDateTime(key: String, default: DateTime? = null): Lazy<DateTime> = lazy(NONE) { intent?.extras?.getString(key)?.parseDateTime() ?: default ?: throw Error("No value $key") }
 fun Activity.stringExtra(key: String, default: String? = null): Lazy<String> = lazy(NONE) { intent?.extras?.getString(key) ?: default ?: throw Error("No value $key") }
-
 fun <T : Parcelable> Fragment.extra(key: String, default: T? = null): Lazy<T> = lazy(NONE) { arguments?.getParcelable<T>(key) ?: default ?: throw Error("No value $key") }
-fun Fragment.extraLocalDate(key: String, default: LocalDate? = null): Lazy<LocalDate> = lazy(NONE) { arguments?.getCharSequence(key).toString().parseLocalDate() ?: default ?: throw Error("No value $key") }
