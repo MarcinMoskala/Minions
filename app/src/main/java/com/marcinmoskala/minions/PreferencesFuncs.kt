4ce08f1a875de1a830dfaddd32f1c085dabc39c6
MarcinMoskala <marcinmoskala@gmail.com>
2017-01-28 08:46:19 +0100

    Make nullabe preferences

--- app/src/main/java/com/marcinmoskala/minions/PreferencesFuncs.kt
+++ app/src/main/java/com/marcinmoskala/minions/PreferencesFuncs.kt
@@ -66,3 +66,49 @@ class PreferenceFieldBinder<T : Any>(val clazz: KClass<T>, val default: T?, val 
 
     private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
 }
+
+inline fun <reified T : Any> bindToPreferenceField(key: String? = null) = PreferenceFieldBinderNullable(T::class, key)
+
+@Suppress("UNCHECKED_CAST")
+class PreferenceFieldBinderNullable<T : Any>(val clazz: KClass<T>, val key: String? = null) : ReadWriteProperty<SharedPreferences, T?> {
+
+    override operator fun getValue(thisRef: SharedPreferences, property: KProperty<*>): T? = readValue(property, thisRef)
+
+    override fun setValue(thisRef: SharedPreferences, property: KProperty<*>, value: T?) {
+        if (value == null) {
+            removeValue(property, thisRef)
+        } else {
+            saveValue(property, thisRef, value)
+        }
+    }
+
+    private fun readValue(property: KProperty<*>, thisRef: SharedPreferences): T? {
+        val key = getKey(property)
+        if (!thisRef.contains(key)) return null
+        return when (clazz.simpleName) {
+            "Long" -> thisRef.getLong(key, -1L) as? T
+            "Int" -> thisRef.getInt(key, -1) as? T
+            "String" -> thisRef.getString(key, null) as? T
+            "Boolean" -> thisRef.getBoolean(key, false) as? T
+            else -> thisRef.getString(key, null)?.fromJson(clazz)
+        }
+    }
+
+    private fun removeValue(property: KProperty<*>, thisRef: SharedPreferences) {
+        thisRef.edit().remove(getKey(property)).apply()
+    }
+
+    private fun saveValue(property: KProperty<*>, thisRef: SharedPreferences, value: T?) {
+        thisRef.edit().apply {
+            when (clazz.simpleName) {
+                "Long" -> putLong(getKey(property), value as Long)
+                "Int" -> putInt(getKey(property), value as Int)
+                "String" -> putString(getKey(property), value as String?)
+                "Boolean" -> putBoolean(getKey(property), value as Boolean)
+                else -> putString(getKey(property), value.toJson())
+            }
+        }.apply()
+    }
+
+    private fun getKey(property: KProperty<*>) = key ?: "${property.name}Key"
+}
--- test/src/main/java/com/marcinmoskala/test/MainActivity.kt
+++ test/src/main/java/com/marcinmoskala/test/MainActivity.kt
@@ -6,15 +6,34 @@ import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import com.marcinmoskala.minions.extra
 import com.marcinmoskala.minions.extraNullable
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import kotlin.properties.Delegates
 
+open class BaseActivity(): AppCompatActivity() {}
 
-class MainActivity : AppCompatActivity() {
+interface MainView {
+    var title: String
+}
 
-//    val a: Parcelable by extra("A")
-//    val b: Parcelable? by extraNullable("A")
+class MainActivity : BaseActivity(), MainView {
+
+    override var title by Delegates.observable("") { prop, old, new ->
+        textView.text = new
+    }
+
+    val presenter by lazy { MainPresenter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter.onStart()
+    }
+}
+
+class MainPresenter(val view: MainView) {
+
+    fun onStart() {
+        view.title = "Hello property"
     }
 }
